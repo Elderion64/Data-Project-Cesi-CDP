aa0e6d48e53951588cfb16f245d1c23d7f80d3a7
antoine pestel <antoine.pestel@viacesi.fr>
2019-07-02 09:18:50 +0200

    test AP

--- matrice.py
+++ matrice.py
@@ -10,7 +10,7 @@ Created on Tue Jul  2 01:27:53 2019
 from random import seed
 from random import randint
 import numpy as np
-import matplotlib.pyplot as plt
+"""import matplotlib.pyplot as plt"""
 
 "Le seed va nous permettre de recréer la même situation si on la rappelle"
 seed(1)
@@ -22,6 +22,7 @@ matrice = np.random.randint(0,50,size=(rand,rand))
 matrice_symm = (matrice + matrice.T)
 np.fill_diagonal(matrice_symm,0)
 print (matrice_symm)
-
+"""
 plt.plot(matrice_symm)
 plt.show
+""""

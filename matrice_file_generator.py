3979939ac4ffda8da875ad961abf9c8a1b3fd9a5
Elderion64 <antoine.barachet@viacesi.fr>
2019-07-03 09:32:48 +0200

    Pénurie d'essence, création de csv & recherche du trajet le plus court

--- matrice_file_generator.py
+++ matrice_file_generator.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from random_matrice import Cities, MatEssence, MatRoad
+import csv
+from random_matrice import Cities, MatRoad
 
 
-with open('data.txt', 'w') as writeFile :
-    writeFile.write('Liste des villes\n\n')
-    writeFile.writelines(Cities.cities_list)
-    writeFile.write('\n\n')
+with open('data.txt', 'w') as txtFile :
+    txtFile.write('Liste des villes\n\n')
+    txtFile.writelines(Cities.cities_list)
+    txtFile.write('\n\n')
     
-    writeFile.write('Matrice de la distance entre les villes (en km)\n\n')
-    np.savetxt(writeFile, MatRoad.matrice_symm, fmt='%d', delimiter='   ', newline='\n')
-    writeFile.write('\n')
-    writeFile.write('Matrice de la consomation d\'essence entre les villes (valeur multiplicatrice)\n\n')
-    np.savetxt(writeFile, MatEssence.matrice_essence_symm, fmt='%1.1f', delimiter='  ', newline='\n')
-
-
-writeFile.close()
+    txtFile.write('Matrice de la distance entre les villes (en km)\n\n')
+    np.savetxt(txtFile, MatRoad.matrice_symm, fmt='%1.1f', delimiter='   ', newline='\n')
 
 
 
+txtFile.close()
+
+with open('distance.csv', 'w') as csvFile :
+    writer = csv.writer(csvFile, delimiter=';')
+    writer.writerow(Cities.cities_list)
+    np.savetxt(csvFile, MatRoad.matrice_symm, fmt='%1.1f', delimiter=';', newline='\n')
+    
+csvFile.close()
--- random_matrice.py
+++ random_matrice.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 "Pour pouvoir générer notre dataset des villes aléatoire"
 import numpy as np
+"Penser à faire 'pip install Faker' pour installer faker"
 from faker import Faker
 "import matplotlib.pyplot as plt"
 
 
 "Le seed va nous permettre de recréer la même situation si on la rappelle"
-np.random.seed(1)
-rand = np.random.randint(1,1000)
+np.random.seed(9)
+rand = np.random.randint(1,100)
+
 """
 print(rand)
 print("")
 """
 
 class MatRoad :
+    citiesR = rand
     "randint c'est un random d'entier et matrice.T est l'inverse de la matrice"
-    matrice = np.random.randint(50,100,size=(rand,rand))
+    matrice = np.random.randint(10,50,size=(citiesR,citiesR))
     matrice_symm = (matrice + matrice.T)/2
     np.fill_diagonal(matrice_symm,0)
     """
@@ -23,23 +26,14 @@ class MatRoad :
     print("")
     """
 
-class MatEssence :
-    matrice_essence = np.random.uniform(low=0.5,high=1.5,size=(rand,rand))
-    matrice_essence_symm = (matrice_essence + matrice_essence.T)/2
-    np.fill_diagonal(matrice_essence_symm,0)
-    matrice_essence_symm = matrice_essence_symm.round(decimals=1)
-    """
-    print (matrice_essence_symm)
-    print("")
-    """
-
 class Cities :
+    citiesR = rand
     fake = Faker()
     cities_list = []
     i = 0
-    fake.seed(1)
+    fake.seed(9)
     
-    while i < rand :
+    while i < citiesR :
         city = fake.city();
         cities_list.append(city);
         i += 1
